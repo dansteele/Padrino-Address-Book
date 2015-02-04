6fee6e410169de934788a55b45d54ea847a6e664
dansteele <danielsteele@hotmail.co.uk>
2015-02-04 16:54:49 +0000

    Basic CR of CRUD implemented for the Person data.

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -1,9 +1,16 @@
 AddressBook::App.controllers :person do
 
-  get "/" do
-
-    "Hello"
-
+  get '/all' do
+    @people = Person.all
+    render 'people/all'
   end
+
+  get "/:first_name" do
+    @person = Person.find_by_first_name(params[:first_name])
+    #binding.pry
+    render 'people/person'
+  end
+
+
 
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,8 +1,26 @@
 require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
+require 'pry'
 
 describe "Person Model" do
   it 'can construct a new instance' do
     @person = Person.new
     refute_nil @person
   end
+
+  it "should list all the people" do
+    @person1 = Person.create(:first_name => "Dan")
+    @person2 = Person.create(:first_name => "Glykeria")
+    get '/person/all'
+    assert last_response.ok?
+    last_response.body.must_match(/Dan/)
+    last_response.body.must_match(/Glykeria/)
+  end
+
+  it "should show an individual person" do
+    Person.create(:first_name => "Dan", :last_name => "Steele")
+    get '/person/Dan'
+    assert last_response.ok?
+    last_response.body.must_match(/Dan/)
+  end
+
 end
